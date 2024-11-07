6a897007ed6b6e760d1440b5692595204fd6f238
yuchan <73932179+oyc0401@users.noreply.github.com>
2024-11-07 15:59:37 +0000

    투명배경 해제하면 무조건 투명색이 흰색되게 함

--- src/functions.js
+++ src/functions.js
@@ -3075,12 +3075,12 @@ function make_opaque() {
 		main_ctx.save();
 		main_ctx.globalCompositeOperation = "destination-atop";
 
-		main_ctx.fillStyle = selected_colors.background;
+		main_ctx.fillStyle = 'white';
 		main_ctx.fillRect(0, 0, main_canvas.width, main_canvas.height);
 
 		// in case the selected background color is transparent/translucent
-		main_ctx.fillStyle = "white";
-		main_ctx.fillRect(0, 0, main_canvas.width, main_canvas.height);
+		// main_ctx.fillStyle = "white";
+		// main_ctx.fillRect(0, 0, main_canvas.width, main_canvas.height);
 
 		main_ctx.restore();
 	});
