3272e4228e0a7fe59557d4fd2ac3409a633e5bd9
yuchan <73932179+oyc0401@users.noreply.github.com>
2024-11-13 15:16:17 +0000

    add menu

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -31,13 +31,44 @@
       TOOL_SELECT,
       TOOL_TEXT,
    } from "../paint/tools";
-
+     import { onMount } from 'svelte';
+   
    function setTool(toolId){
       console.log(toolId);
-      window.select_tool(window.get_tool_by_id(toolId))
-         //  window.globAppstate.selected_tool = window.get_tool_by_id(toolId);
-         // window.globAppstate.selected_tools = [window.globAppstate.selected_tool]
+      window.select_tool(window.get_tool_by_id(toolId));
    }
+   
+   //////
+
+    
+     let showMenu = false;
+     let button;
+     let menuPosition = { top: 0, left: 0 };
+
+     const toggleMenu = () => {
+       showMenu = !showMenu;
+       if (showMenu && button) {
+         const rect = button.getBoundingClientRect();
+         menuPosition = {
+           top: rect.bottom + window.scrollY,
+           left: rect.left + window.scrollX
+         };
+       }
+     };
+
+     const closeMenu = () => (showMenu = false);
+
+     // 외부 클릭 시 메뉴를 닫도록 설정
+     onMount(() => {
+       const handleClickOutside = (event) => {
+         if (showMenu && !event.target.closest('.menu') && !event.target.closest('button')) {
+           closeMenu();
+         }
+       };
+       document.addEventListener('click', handleClickOutside);
+       return () => document.removeEventListener('click', handleClickOutside);
+     });
+   
 </script>
 
 <header>
@@ -55,7 +86,10 @@
       </div>
    </div>
    <div class="menus">
-      <div class="menu-button"><img src={MenuIcon} alt="menu" /></div>
+      <button class="menu-button"
+         bind:this={button} on:click={toggleMenu}>
+         <img src={MenuIcon} alt="menu" />
+      </button>
       <div class="menu-button" on:click={()=>{setTool(TOOL_SELECT)}}><img src={SelectionIcon} alt="selection" /></div>
       <div class="menu-button" on:click={()=>{setTool(TOOL_PENCIL)}}><img src={PenIcon} alt="tools" /></div>
       <div class="menu-button" on:click={()=>{setTool(TOOL_BRUSH)}}><img src={BrushIcon} alt="brush" /></div>
@@ -66,8 +100,29 @@
       <div class="menu-button">
          <img src={FullScreenIcon} alt="full-screen" />
       </div>
+      {#if showMenu}
+          <div
+            class="menu show"
+            style="top: {menuPosition.top}px; left: {menuPosition.left}px;"
+          >
+            <p>메뉴 내용</p>
+          </div>
+        {/if}
    </div>
+  
 </header>
 
-<style lang="postcss">
+<style>
+    .menu {
+       position: absolute;
+       background-color: #fff;
+       border: 1px solid #ccc;
+       padding: 8px;
+       display: none;
+       z-index: 10;
+     }
+
+     .menu.show {
+       display: block;
+     }
 </style>
