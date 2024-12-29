0048eb50e19d6f8c0244ed9fe0bc832d00e0776b
yuchan <73932179+oyc0401@users.noreply.github.com>
2024-12-29 07:39:37 +0000

    리팩토링시 오류 수정

--- src/paint/app.js
+++ src/paint/app.js
@@ -238,7 +238,64 @@ export function initApp(canvasAreaQuery) {
 
   // #endregion
 
- function manageKeyboard(){
+
+   const nextZoom = {
+     0.5: 1,
+     1: 2,
+     2: 3,
+     3: 4,
+     4: 5,
+     5: 6,
+     6: 8,
+     8: 10,
+     10: 12,
+     12: 15,
+     15: 20,
+     20: 25,
+     25: 30,
+     30: 40,
+     40: 50,
+     50: 60,
+     60: 75,
+     75: 100,
+     100: 100,
+   };
+
+   const nextout = {
+     100: 75,
+     75: 60,
+     60: 50,
+     50: 40,
+     40: 30,
+     30: 25,
+     25: 20,
+     20: 15,
+     15: 12,
+     12: 10,
+     10: 8,
+     8: 6,
+     6: 5,
+     5: 4,
+     4: 3,
+     3: 2,
+     2: 1,
+     1: 0.5,
+     0.5: 0.5,
+   };
+
+  function getClosestZoom(currentZoom) {
+     const zoomLevels = Object.keys(nextZoom)
+       .map(Number)
+       .sort((a, b) => a - b);
+     for (let i = zoomLevels.length - 1; i >= 0; i--) {
+       if (currentZoom >= zoomLevels[i]) {
+         return zoomLevels[i];
+       }
+     }
+     return zoomLevels[0]; // 만약 currentZoom이 가장 낮은 줌보다 작다면 최소값 반환
+   }
+
+  function manageKeyboard(){
    // #region Keyboard Shortcuts
    $(window).on("keydown", (e) => {
      // typecast to HTMLElement because e.target is incorrectly given as Window, due to wrapping window
@@ -514,61 +571,6 @@ export function initApp(canvasAreaQuery) {
    // #endregion
 
 
-   const nextZoom = {
-     0.5: 1,
-     1: 2,
-     2: 3,
-     3: 4,
-     4: 5,
-     5: 6,
-     6: 8,
-     8: 10,
-     10: 12,
-     12: 15,
-     15: 20,
-     20: 25,
-     25: 30,
-     30: 40,
-     40: 50,
-     50: 60,
-     60: 75,
-     75: 100,
-     100: 100,
-   };
-
-   const nextout = {
-     100: 75,
-     75: 60,
-     60: 50,
-     50: 40,
-     40: 30,
-     30: 25,
-     25: 20,
-     20: 15,
-     15: 12,
-     12: 10,
-     10: 8,
-     8: 6,
-     6: 5,
-     5: 4,
-     4: 3,
-     3: 2,
-     2: 1,
-     1: 0.5,
-     0.5: 0.5,
-   };
-
-   function getClosestZoom(currentZoom) {
-     const zoomLevels = Object.keys(nextZoom)
-       .map(Number)
-       .sort((a, b) => a - b);
-     for (let i = zoomLevels.length - 1; i >= 0; i--) {
-       if (currentZoom >= zoomLevels[i]) {
-         return zoomLevels[i];
-       }
-     }
-     return zoomLevels[0]; // 만약 currentZoom이 가장 낮은 줌보다 작다면 최소값 반환
-   }
 
    addEventListener(
      "wheel",
