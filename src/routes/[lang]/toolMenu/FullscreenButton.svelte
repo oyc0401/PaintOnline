ad18af09fb56a3d2d66ecbccab8f67cde3456812
yuchan <73932179+oyc0401@users.noreply.github.com>
2025-01-01 14:13:09 +0000

    전체화면 코드 삽입

--- src/routes/[lang]/toolMenu/FullscreenButton.svelte
+++ src/routes/[lang]/toolMenu/FullscreenButton.svelte
@@ -3,8 +3,38 @@
 
   import "./menu.css";
   import "../toolsMenu.css";
+
+  function setFullscreen() {
+    // 전체 화면으로 만들 요소 (여기서는 <html> 전체)
+    const element = document.documentElement;
+    if (element.requestFullscreen) {
+      element.requestFullscreen(); // 표준 방식
+    } else if (element.webkitRequestFullscreen) {
+      element.webkitRequestFullscreen(); // Safari
+    } else if (element.msRequestFullscreen) {
+      element.msRequestFullscreen(); // IE
+    }
+  }
+
+  function exitFullscreen() {
+    if (document.exitFullscreen) {
+      document.exitFullscreen(); // 표준 방식
+    } else if (document.webkitExitFullscreen) {
+      document.webkitExitFullscreen(); // Safari
+    } else if (document.msExitFullscreen) {
+      document.msExitFullscreen(); // IE
+    }
+  }
+
+  function toggleFullscreen() {
+    if (document.fullscreenElement || document.webkitFullscreenElement || document.msFullscreenElement) {
+      exitFullscreen();
+    } else {
+      setFullscreen();
+    }
+  }
 </script>
 
-<div class="menu-button">
-  <FullScreenIcon/>
-</div>
+<button class="menu-button" onclick={toggleFullscreen}>
+  <FullScreenIcon />
+</button>
